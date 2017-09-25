9b8c727d69b47984da020d523bf29e4cf65199c9
w326004741 <G00329371@gmit.ie>
2017-09-25 11:53:05 +0100

    git push

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,14 @@
 # FizzBuzz
 
-for n in range(1,21):
-    string = ""
-    if n % 3 == 0:
-        string = string + "Fizz"
-    if n % 5 == 0:
-        stirng = string + "Buzz"
-    if n % 5 != 0 and n % 3 != 0:
-        string = string  + str(n)
-    print(string)
+
+# Using python range() function to create a list of numbers [0-99] so we can using  a for loop over it
+for n in range(0,100):
+    
+    if n % 3 ==0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
