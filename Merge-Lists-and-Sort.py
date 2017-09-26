bdc10fd78d041a0c943a71d89a249b58b246f1b0
w326004741 <G00329371@gmit.ie>
2017-09-26 21:43:35 +0100

    git push

--- Merge-Lists-and-Sort.py
+++ Merge-Lists-and-Sort.py
@@ -10,19 +10,22 @@ list3 = []
 print("Please enter 3 numbers for each list")
 
 print("List 1")
-for i in range (0,3): #using for loop set need enter float type number three times and append to the listItem as list1 
+#using for loop set need enter float type number three times and append to the listItem as list1 
+for i in range (0,3): 
     listItem = float(input(str(i + 1) + ": "))
     list1.append(listItem)
 
-print("List 2") #using for loop set need enter number three times and append to the listItem as list2
+#using for loop set need enter number three times and append to the listItem as list2
+print("List 2") 
 for i in range (0,3):
     listItem = float(input(str(i + 1) + ": "))
     list2.append(listItem)
 
-list3 = list1 + list2 # Merge list1 and list2
+# Merge list1 and list2
+list3 = list1 + list2 
 
 print("List 1: " + str(list1))
 print("List 2: " + str(list2))
 print("List 3: " + str(list3))
-print("Sorted List: " + str(sorted(list3))) # sorted() funtion is sort the list3
-
+# sorted() funtion is sort the list3
+print("Sorted List: " + str(sorted(list3))) 
